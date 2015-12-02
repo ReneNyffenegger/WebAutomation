9c079538eaa2d2a3af0b93bf8e9654ecb378450a
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-12-02 21:59:14 +0100

    + 020_follow_link.pl

---
+++ WWW-Mechanize-Firefox/020_follow_link.pl
@@ -0,0 +1,18 @@
+use warnings;
+use strict;
+
+use WWW::Mechanize::Firefox;
+
+my $mech = new WWW::Mechanize::Firefox(
+  create    => 1,   # Create new tab...
+  activate  => 1,   # ... and activate it.
+  autoclose => 0,   # Keep tab open after perl script end.
+# --------------------------------------------------------
+# Name of the program if unable to connect on first try:
+  launch   =>'C:\Program Files\Mozilla Firefox\firefox.exe',
+);
+
+
+$mech->get('http://localhost/WebAutomation/index.html');
+
+$mech->click({xpath => '/html/body/a'});
